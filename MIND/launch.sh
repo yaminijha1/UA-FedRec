9181a1ca70cfdcb2d9cde3a3caa6c1afb95304e0
Yamini Jha <yaminijhabhl@gmail.com>
2024-04-01 08:12:27 +0530

    modify lanch file

--- MIND/launch.sh
+++ MIND/launch.sh
@@ -1,15 +1,15 @@
-cd raw 
+# cd raw 
 # ./download.sh mind .
 # ./download.sh glove .
 # ./download.sh feeds .
 
-cd ../
+# cd ../
 
 cd preprocess
 python news_glove_process.py --data mind
 python news_popular.py --data mind
 python user_process.py --data mind
 
-python news_glove_process.py --data feeds --min_word_cnt 10
-python news_popular.py --data feeds
-python user_process.py --data feeds
+# python news_glove_process.py --data feeds --min_word_cnt 10
+# python news_popular.py --data feeds
+# python user_process.py --data feeds
